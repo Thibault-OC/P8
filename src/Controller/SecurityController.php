d90d1aeb593078919c6f008b14009f641d53428d
Thibault-OC <vacheron.thibault@gmail.com>
2021-05-21 12:16:26 +0200

    add login controller

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -37,12 +37,6 @@ class SecurityController extends AbstractController
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 
-    /**
-     * @Route("/login_check", name="login_check")
-     */
-    public function loginCheck()
-    {
-        // This code is never executed.
-    }
+
 
 }
